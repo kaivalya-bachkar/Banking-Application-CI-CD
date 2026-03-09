c8337deb06885980f34522b9611c09bb4ca5b7eb
kaivalya-bachkar <kaivalya.bachkar@gmail.com>
2026-03-09 12:17:16 +0530

    added a infrastructure code

---
+++ infrastructure/main.tf
@@ -0,0 +1,4 @@
+module "vn" {
+  source = "./modules/vn"
+
+}
---
+++ infrastructure/modules/vn/main.tf
@@ -0,0 +1,172 @@
+#vnet---------------------------------------------
+resource "azurerm_resource_group" "rg" {
+  name     = "var.App_name"
+  location = "Central India"
+}
+
+# resource "azurerm_network_security_group" "example" {
+#   name                = "example-security-group"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "var.vnet-name"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = "var.vnet_cidr"
+
+  #   subnet {
+  #     name             = "subnet1"
+  #     address_prefixes = ["10.0.1.0/24"]
+  #   }
+
+  #   subnet {
+  #     name             = "subnet2"
+  #     address_prefixes = ["10.0.2.0/24"]
+  #     security_group   = azurerm_network_security_group.example.id
+  #   }
+
+}
+#subnets------------------------------------------------------------
+# resource "azurerm_resource_group" "example" {
+#   name     = "example-resources"
+#   location = "West Europe"
+# }
+
+# resource "azurerm_virtual_network" "example" {
+#   name                = "example-vnet"
+#   address_space       = ["10.0.0.0/16"]
+#   location            = azurerm_resource_group.example.location
+#   resource_group_name = azurerm_resource_group.example.name
+# }
+
+resource "azurerm_subnet" "public" {
+  count                = length(var.public_subnet_cidrs)
+  name                 = "public-subnet-${count.index + 1}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.public_subnet_cidrs[count.index]]
+}
+
+resource "azurerm_subnet" "private" {
+  count                = length(var.private_subnet_cidrs)
+  name                 = "private-subnet-${count.index + 1}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.private_subnet_cidrs[count.index]]
+}
+delegation {
+  name = "fs"
+  service_delegation {
+    name = "Microsoft.DBforPostgreSQL/flexibleServers"
+    actions = [
+      "Microsoft.Network/virtualNetworks/subnets/join/action",
+    ]
+  }
+}
+
+#public nsg for application gateway
+resource "azurerm_network_security_group" "public_nsg" {
+  name                = "var.public_nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-HTTP-Inbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-AppGW-Manager"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "65200-65535"
+    source_address_prefix      = "GatewayManager"
+    destination_address_prefix = "*"
+  }
+}
+
+#private nsg for acr, aks, and db
+resource "azurerm_network_security_group" "private_nsg" {
+  name                = "var.private_nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-VNet-Internal"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "VirtualNetwork"
+  }
+
+  security_rule {
+    name                       = "Deny-Internet-Inbound"
+    priority                   = 4096
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "public" {
+  count                     = length(var.public_subnet_cidrs)
+  subnet_id                 = azurerm_subnet.public[count.index].id
+  network_security_group_id = azurerm_network_security_group.public_nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "private" {
+  count                     = length(var.private_subnet_cidrs)
+  subnet_id                 = azurerm_subnet.private[count.index].id
+  network_security_group_id = azurerm_network_security_group.private_nsg.id
+}
+#nat gateway--------------------------------------------------------
+# resource "azurerm_resource_group" "example" {
+#   name     = "example-resources"
+#   location = "West Europe"
+# }
+
+resource "azurerm_public_ip" "nat_ip" {
+  name                = "var.public_nat_ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_nat_gateway" "nat_gw" {
+  name                = "var.nat_gw_name"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "Standard"
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "nat_assoc" {
+  nat_gateway_id       = azurerm_nat_gateway.nat_gw.id
+  public_ip_address_id = azurerm_public_ip.nat_ip.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "private" {
+  count          = length(var.private_subnet_cidrs)
+  subnet_id      = azurerm_subnet.private[count.index].id
+  nat_gateway_id = azurerm_nat_gateway.nat_gw.id
+}
